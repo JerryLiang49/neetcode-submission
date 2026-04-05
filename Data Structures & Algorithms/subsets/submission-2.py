01eb610a83ed40190171533f13f80aaab856ad99
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-04-05 08:26:07 +0800

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+
+        def dfs(index, path):
+            result.append(path[:])
+            for i in range(index, len(nums)):
+                path.append(nums[i])
+                dfs(i + 1, path)
+                path.pop()
+            
+        dfs(0, [])
+        return result
