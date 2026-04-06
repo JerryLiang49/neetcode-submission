9db9dc1afb07390eae5f7eb08881eb580a3f0117
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-04-06 09:22:17 +0800

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def dfs(r, c, index):
+            nonlocal visited 
+
+            if index == len(word):
+                return True
+
+            if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]):
+                return False
+            
+            if board[r][c] != word[index] or (r, c) in visited:
+                return False
+            
+            visited.add((r, c))
+            result = (dfs(r + 1, c, index + 1) or dfs(r - 1, c, index + 1) or
+                        dfs(r, c + 1, index + 1) or dfs(r, c - 1, index + 1))
+            visited.remove((r, c))
+            return result
+        
+        visited = set()
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if dfs(r, c, 0):
+                    return True
+        return False
