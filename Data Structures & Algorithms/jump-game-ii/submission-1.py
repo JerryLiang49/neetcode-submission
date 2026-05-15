47a913b4c4ad175936f727e428b2f75e939738c7
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-05-15 13:44:13 +0800

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        furthest = 0
+        jumps = 0
+
+        while r < len(nums) - 1:
+            for i in range(l, r + 1):
+                furthest = max(furthest, i + nums[i])
+            jumps += 1
+            l = r + 1
+            r = furthest
+        return jumps
