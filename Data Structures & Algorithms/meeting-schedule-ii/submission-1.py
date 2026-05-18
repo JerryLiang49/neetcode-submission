aa552337e33fc4b9ff7fb013b15d207b5f706f5f
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-05-18 14:46:52 +0800

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,32 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+
+        start = []
+        end = []
+
+        for interval in intervals:
+            start.append(interval.start)
+            end.append(interval.end)
+        start.sort()
+        end.sort()
+
+        count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                count += 1
+            else:
+                e += 1
+            s += 1
+
+        return count
