f4ff6eeeb6362f49d31afe4680bc536d82c14308
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-06-01 00:53:38 +0800

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        power = 1
+
+        for i in range(1, n + 1):
+            if power * 2 == i:
+                power = i
+            dp[i] = 1 + dp[i - power]
+        
+        return dp
