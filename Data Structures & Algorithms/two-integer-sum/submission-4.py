ee443e849de661873cdfc656fa9132ff52d258af
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-06-08 06:17:46 +0800

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i, num in enumerate(nums):
+            find = target - num
+            if find in d:
+                return [d[find], i]
+            d[num] = i
