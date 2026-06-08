e39f92e2bd7ca6ff9e7f55bde0c4a33db8132c1c
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-06-08 10:36:15 +0800

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        k = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+                k += 1
+        return k
