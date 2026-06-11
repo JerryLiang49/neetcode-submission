114270fbc4141346dee7cf5a0502b34a37c22fd5
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-06-12 07:08:47 +0800

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i = 0
+        j = len(people) - 1
+        boats = 0
+
+        while i <= j:
+            curr = people[i] + people[j]
+            if curr > limit:
+                boats += 1
+                j -= 1
+            else:
+                boats += 1
+                i += 1
+                j -= 1
+
+        return boats
