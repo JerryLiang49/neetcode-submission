0dcc680bd53882553f4eb253279343bc92e8ace0
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-06-20 09:45:51 +0800

    Add: split-array-largest-sum - submission-2

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        
+        def split(largest):
+            curr = 0
+            splits = 0
+            for num in nums:
+                curr += num
+                if curr > largest:
+                    splits += 1
+                    curr = num
+            return splits + 1
+        
+        l = max(nums)
+        r = sum(nums)
+        while l <= r:
+            mid = (l + r) // 2
+            splits = split(mid)
+
+
+            if splits > k:
+                l = mid + 1
+            else:
+                result = mid
+                r = mid - 1
+        
+        return result
