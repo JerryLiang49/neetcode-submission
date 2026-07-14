31acef3bc1acb02e54afb351c1a424ea6269b418
Zhanming (Jerry) Liang <zhanminl@andrew.cmu.edu>
2026-07-14 12:55:50 +0800

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.py
@@ -0,0 +1,24 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+
+    def addNum(self, num: int) -> None:
+        if self.minHeap and num > self.minHeap[0]:
+            heapq.heappush(self.minHeap, num)
+        else:
+            heapq.heappush(self.maxHeap, -1 * num)
+        
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+
+    def findMedian(self) -> float:
+        if len(self.maxHeap) > len(self.minHeap):
+            return -1 * self.maxHeap[0]
+        elif len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        return (-1 * self.maxHeap[0] + self.minHeap[0]) / 2.0
+    
